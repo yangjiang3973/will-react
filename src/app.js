f9046df9295c4774cb32fd85fbe2bf1d4e7dab85
Yang Jiang <yang.y.jiang@ibm.com@yangs-mbp.spa.can.ibm.com>
2018-06-05 17:11:39 -0400

    TODO: build DOM tree for children

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
+// const Hello = function () {
+//     return React.createElement('div', null, `aaa World`);
+// };
+// const helloWorld = React.createElement(Hello, null, null);
+// ReactDOM.render(helloWorld, document.getElementById('root'));
+
+
+
 const Hello = function () {
-    return React.createElement('div', null, `aaa World`);
+    return React.createElement('div', null, `Hello World`);
 };
 const helloWorld = React.createElement(Hello, null, null);
-ReactDOM.render(helloWorld, document.getElementById('root'));
+const helloWorld2 = React.createElement(Hello, null, null);
+const regularDiv = React.createElement('div', null, `I'm just a regular div`);
+
+const parent = React.createElement('div', null,
+        helloWorld,
+        helloWorld2,
+        regularDiv,
+        ` I'm just a text`
+);
+// We expect to see:
+// -----------------
+// Hello World
+// Hello World
+// I'm just a regular div
+// I'm just a text
+ReactDOM.render(parent, document.getElementById('root'));
--- src/react.js
+++ src/react.js
@@ -29,4 +29,19 @@
 //     return React.createElement('div', null, `Hello World`);
 // };
 // const helloWorld = React.createElement(Hello, null, null);
-// ReactDOM.render(helloWorld, document.getElementById('root'));
+// const helloWorld2 = React.createElement(Hello, null, null);
+// const regularDiv = React.createElement('div', null, `I'm just a regular div`);
+//
+// const parent = React.createElement('div', null,
+//         helloWorld,
+//         helloWorld2,
+//         regularDiv,
+//         ` I'm just a text`
+// );
+// // We expect to see:
+// // -----------------
+// // Hello World
+// // Hello World
+// // I'm just a regular div
+// // I'm just a text
+// ReactDOM.render(parent, document.getElementById('root'));
