6a992bd2df8827ab01d729fb7455c7ac28f89b3c
Yang Jiang <yang.y.jiang@ibm.com@yangs-mbp.spa.can.ibm.com>
2018-06-05 16:59:59 -0400

    support for react component

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-const helloWorld = React.createElement('div', null, `Hello World`);
-
+const Hello = function () {
+    return React.createElement('div', null, `aaa World`);
+};
+const helloWorld = React.createElement(Hello, null, null);
 ReactDOM.render(helloWorld, document.getElementById('root'));
--- src/react.js
+++ src/react.js
@@ -1,8 +1,13 @@
 (() => {
   function anElement(element, children) {
-    const anElement = document.createElement(element);
-    anElement.innerHTML = children.join(' ');
-    return anElement;
+    if (typeof element === 'function') {
+        return element();
+    }
+    else {
+        const anElement = document.createElement(element);
+        anElement.innerHTML = children.join(' ');
+        return anElement;
+    }
   }
 
   function createElement(el, props, ...children) {
@@ -18,3 +23,10 @@
     }
   };
 })();
+
+
+// const Hello = function () {
+//     return React.createElement('div', null, `Hello World`);
+// };
+// const helloWorld = React.createElement(Hello, null, null);
+// ReactDOM.render(helloWorld, document.getElementById('root'));
