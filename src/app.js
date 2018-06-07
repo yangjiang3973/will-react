5c7fe2be86331ec94bb7abdc3867aa15b81244ed
Yang Jiang <yang.y.jiang@ibm.com@yangs-mbp.spa.can.ibm.com>
2018-06-07 16:53:53 -0400

    TODO: add state to make app work

--- src/app.js
+++ src/app.js
@@ -45,17 +45,43 @@
 // const helloWorld = React.createElement(Hello, {name: 'yang'}, null);
 
 
-class MyButton extends React.Component {
+// class MyButton extends React.Component {
+//
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render() {
+//     return React.createElement('button', {onClick: this.props.onClick}, `Click me`);
+//   }
+// }
+// const myBtn = React.createElement(MyButton, {onClick: () => alert('yay it worked')}, null);
+// ReactDOM.render(myBtn, document.getElementById('root'));
 
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    return React.createElement('button', {onClick: this.props.onClick}, `Click me`);
-  }
+class Counter extends React.Component {
+     constructor(props) {
+         super(props);
+         this.state = {value: 0};
+     }
+
+     onPlusClick() {
+         this.setState({value: this.state.value + 1});
+     }
+
+     onMinusClick() {
+         this.setState({value: this.state.value - 1});
+     }
+
+     render() {
+         return React.createElement('div', null,
+            React.createElement('div', null, 'A Counter'),
+            React.createElement('div', null, {this.state.value}),
+            React.createElement('button', {onClick: this.onPlusClick.bind(this)}, '+'),
+            React.createElement('button', {onClick: this.onMinusClick.bind(this)}, '-')
+        );
+     }
 }
-const myBtn = React.createElement(MyButton, {onClick: () => alert('yay it worked')}, null);
-ReactDOM.render(myBtn, document.getElementById('root'));
 
-// ReactDOM.render(helloWorld, document.getElementById('root'));
+const counter = React.createElement(Counter, null, null);
+ReactDOM.render(counter, document.getElementById('root'));
