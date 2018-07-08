81fbcdf9ed93b029f64219aaf5a69294fff4d2a2
Yang Jiang <yangjiang@Yangs-MacBook-Air.local>
2018-07-08 18:43:15 -0400

    Vnode for DOM element

--- app/app.jsx
+++ app/app.jsx
@@ -1,18 +1,35 @@
 import React from '../src/react.js'
 import ReactDOM from '../src/reactDOM.js'
 
-const MyCoolJSXQuoteComponent = ({quote, author}) => {
-  return (
-    <div className="quote-container">
-      <h4 className="quote">"{quote}"</h4>
-      <div className="author">- {author}</div>
-    </div>
-  );
-};
+// const MyCoolJSXQuoteComponent = ({quote, author}) => {
+//   return (
+//     <div className="quote-container">
+//       <h4 className="quote">"{quote}"</h4>
+//       <div className="author">- {author}</div>
+//     </div>
+//   );
+// };
+//
+//
+// ReactDOM.render(
+//   <MyCoolJSXQuoteComponent
+//     quote="The only source of knowledge is experience."
+//     author="Albert Einstein"/>,
+//   document.getElementById('root'));
 
+// TEST 1
+// const helloWorld = <div>hello sb!</div>;
+//
+// ReactDOM.render(helloWorld, document.getElementById('root'));
 
-ReactDOM.render(
-  <MyCoolJSXQuoteComponent
-    quote="The only source of knowledge is experience."
-    author="Albert Einstein"/>,
-  document.getElementById('root'));
+// TEST 2
+// const helloWorld2 = <div style={{background: 'red'}}>
+//                         hello sb!
+//                         <div>
+//                             hello tony!
+//                         </div>
+//                     </div>;
+//
+// ReactDOM.render(helloWorld2, document.getElementById('root'));
+
+// TEST 3
